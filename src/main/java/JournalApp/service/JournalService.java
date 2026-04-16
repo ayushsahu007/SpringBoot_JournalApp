d02a07a0567f18308fa654430eef5157d3a8b42b
ayushsahu007 <ayushsahu007ay@gmail.com>
2026-04-16 11:08:33 +0530

    sonar qube

--- src/main/java/JournalApp/service/JournalService.java
+++ src/main/java/JournalApp/service/JournalService.java
@@ -5,6 +5,8 @@ import JournalApp.entity.User;
 import JournalApp.repository.JournalRepository;
 import JournalApp.repository.UserRepository;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,6 +29,7 @@ public class JournalService {
     @Autowired
     private UserRepository userRepository;
 
+
     @Transactional
     public void saveEntry(JournalEntry journalEntry,String userName){
        try {
@@ -37,6 +40,7 @@ public class JournalService {
            userService.saveEntry(user);
        }catch (Exception e){
             e.printStackTrace();
+
        }
       }
 
--- src/main/java/JournalApp/service/UserService.java
+++ src/main/java/JournalApp/service/UserService.java
@@ -1,7 +1,10 @@
 package JournalApp.service;
 import JournalApp.entity.User;
 import JournalApp.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,6 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class UserService {
 
     @Autowired
@@ -20,10 +24,27 @@ public class UserService {
 
     private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    public void saveNewUser(User user){
-     user.setPassword(passwordEncoder.encode(user.getPassword()));
-     user.setRoles(Arrays.asList("USER"));
-         userRepository.save(user);
+  //  private static final Logger logger = LoggerFactory.getLogger(JournalService.class);
+
+
+    public boolean saveNewUser(User user){
+   try {
+       user.setPassword(passwordEncoder.encode(user.getPassword()));
+       user.setRoles(Arrays.asList("USER"));
+       userRepository.save(user);
+       return true;
+   }catch (Exception e){
+       log.info("Error saving user");
+       log.error("Error saving user");
+
+       log.warn("Error saving user");
+
+       log.debug("Error saving user");
+
+       log.trace("Error saving user");
+
+   }
+        return false;
     }
 
     public void saveAdmin(User user){
